12ca5c135137093d32769cab37c3f8e282dc5dfc
MavisTian <1574731096@qq.com>
2018-12-12 00:20:40 +0800

    init commit

---
+++ admin/js/common.js
@@ -0,0 +1,19 @@
+
+	NProgress.start();
+
+	NProgress.done();
+
+	$('.navs ul').prev('a').on('click', function () {
+		$(this).next().slideToggle();
+	});
+
+	 // 检测用户是否登录
+	 $.ajax({
+		url: '/api/employee/checkRootLogin',
+		type: 'get',
+		success: function (info) {
+			if (info.error) {
+				location.href = '/admin/login.html';
+			}
+		}
+	})
